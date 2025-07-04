f20b1f7d8a785cdb3131a165de79e085cfc46c5a
Mzizi Mzwakhe <mzizimzwakhe@MZIZISs-MacBook-Air.local>
2025-07-04 10:32:04 +0200

    Ensure Tailwind and CSS configs are pushed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import './App.css';
 import { NotificationProvider } from "./components/NotificationContext";
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
